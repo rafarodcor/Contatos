539fc05e47e5706d492513690f3a7511d4fc7f77
Rafael <rafarc@outlook.com>
2025-03-12 22:58:34 -0300

    [UPDATE] Adicionado os arquivos referentes ao OpenTelemetry, Prometheus e Grafana para monitoramento da API.

--- Contatos.API/Program.cs
+++ Contatos.API/Program.cs
@@ -1,8 +1,26 @@
 using Contatos.Dados.Banco;
 using Contatos.Dados.Repositories;
 using Contatos.Services.Services;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add prometheus exporter
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(opt =>
+
+        opt
+            .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("OpenRemoteManage.GatewayAPI"))
+            .AddMeter(builder.Configuration.GetValue<string>("OpenRemoteManageMeterName"))
+            .AddAspNetCoreInstrumentation()
+            .AddRuntimeInstrumentation()
+            .AddProcessInstrumentation()
+            .AddOtlpExporter(opts =>
+            {
+                opts.Endpoint = new Uri(builder.Configuration["Otel:Endpoint"]);
+            })
+    );
 
 // Add services to the container.
 builder.Services.AddDbContext<ContatosContext>();
